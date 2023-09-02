bde47b0722eb613742c5cecb719125e30182887f
Andrey-VU <avustjuzhanin@yandex.ru>
2023-09-02 12:18:31 +0300

    Debug 'em all - Finita la Postman

--- main-service/src/main/java/ru/practicum/user/dto/NewUserRequest.java
+++ main-service/src/main/java/ru/practicum/user/dto/NewUserRequest.java
@@ -2,7 +2,6 @@ package ru.practicum.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.validation.constraints.Email;
